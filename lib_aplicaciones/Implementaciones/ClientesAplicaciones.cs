8ccdaa677835c98312d7d2550f4625fa4b6256ee
arqua <arquantos66@hotmail.com>
2025-06-01 19:00:35 -0500

    modificacion IclientesAplicacion

--- lib_aplicaciones/Implementaciones/ClientesAplicaciones.cs
+++ lib_aplicaciones/Implementaciones/ClientesAplicaciones.cs
@@ -3,36 +3,72 @@ using lib_dominio.Entidades;
 using lib_repositorio.Interfaces;
 
 using Microsoft.EntityFrameworkCore;
+
 namespace lib_aplicaciones.Implementaciones
 {
-    public class NotasAplicacion : INotasAplicacion
+    public class ClientesAplicaciones : IClientesAplicacion
     {
         private IConexion? IConexion = null;
-        public NotasAplicacion(IConexion iConexion)
-        { this.IConexion = iConexion; }
-        public void Configurar(string StringConexion) { this.IConexion!.StringConexion = StringConexion; }
-        public Notas? Borrar(Notas? entidad)
-        {
-            if (entidad == null) throw new Exception("lbFaltaInformacion");
-            if (entidad!.Id == 0) throw new Exception("lbNoSeGuardo");
-            this.IConexion!.Notas!.Remove(entidad); this.IConexion.SaveChanges(); return entidad;
+        public ClientesAplicaciones(IConexion iConexion)
+        { 
+            this.IConexion = iConexion; 
+        
         }
-        public Notas? Guardar(Notas? entidad)
-        {
-            if (entidad == null) throw new Exception("lbFaltaInformacion");
-            if (entidad.Id != 0) throw new Exception("lbYaSeGuardo");
-            entidad!.NotaFinal = (entidad.Nota1 + entidad.Nota2 + entidad.Nota3 + entidad.Nota4 + entidad.Nota5) / 5;
-            this.IConexion!.Notas!.Add(entidad); this.IConexion.SaveChanges(); return entidad;
+        public void Configurar(string StringConexion)
+        { 
+            this.IConexion!.StringConexion = StringConexion; 
         }
-        public List<Notas> Listar() { return this.IConexion!.Notas!.Take(20).ToList(); }
-        public List<Notas> PorEstudiante(Notas? entidad) { return this.IConexion!.Notas!.Where(x => x.Estudiante!.Contains(entidad!.Estudiante!)).ToList(); }
-        public Notas? Modificar(Notas? entidad)
+
+        public Clientes? Borrar(Clientes? entidad)
         {
-            if (entidad == null) throw new Exception("lbFaltaInformacion");
-            if (entidad!.Id == 0) throw new Exception("lbNoSeGuardo");
-            entidad!.NotaFinal = (entidad.Nota1 + entidad.Nota2 + entidad.Nota3 + entidad.Nota4 + entidad.Nota5) / 5;
-            var entry = this.IConexion!.Entry<Notas>(entidad); entry.State = EntityState.Modified; this.IConexion.SaveChanges(); return entidad;
+            if (entidad == null)
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad!.id == 0) 
+                throw new Exception("lbNoSeGuardo");
+
+            this.IConexion!.Clientes!.Remove(entidad);
+            this.IConexion.SaveChanges(); 
+            return entidad;
+        }
+        public Clientes? Guardar(Clientes? entidad)
+        {
+            if (entidad == null)
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad.id != 0)
+                throw new Exception("lbYaSeGuardo");
+
+            
+
+            this.IConexion!.Clientes!.Add(entidad);
+            this.IConexion.SaveChanges();
+            return entidad;
+        }
+        public List<Clientes> Listar()
+        { 
+            return this.IConexion!.Clientes!.Take(20).ToList(); 
+        }
+        public List<Clientes> PorCodigo(Clientes? entidad) 
+        { 
+            return this.IConexion!.Clientes!
+                .Where(x => x.codigo!.Contains(entidad!.codigo!))
+                .ToList(); 
+        }
+        public Clientes? Modificar(Clientes? entidad)
+        {
+            if (entidad == null)
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad!.id == 0) 
+                throw new Exception("lbNoSeGuardo");
+
+            
+
+            var entry = this.IConexion!.Entry<Clientes>(entidad); 
+            entry.State = EntityState.Modified; 
+            this.IConexion.SaveChanges(); 
+            return entidad;
         }
     }
 }
-Lógica
